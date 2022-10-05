e696c91ba3c74f3030c1fed8f2e9cb1f30a17b4d
stvnfrlls <stvnfrlls@gmail.com>
2022-10-05 21:59:03 +0800

    Added functions:
    -getting user details
    -adding of account
    -updating user password

--- models/model.js
+++ models/model.js
@@ -1,1 +1,45 @@
-const DB = require('../util/database');
+const DB = require("../util/database");
+
+module.exports = class User {
+  getUserdetails(email) {
+    return new Promise((resolve, reject) => {
+      DB.execute(`SELECT * FROM userdetails WHERE username = ?`, [email])
+        .then(([rows]) => {
+          resolve(rows);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  addUserdetails(email, password) {
+    return new Promise((resolve, reject) => {
+      DB.execute(
+        `INSERT INTO userdetails(username, password) VALUES ("${email}", "${password}")`
+      )
+        .then(([result]) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  updateUserPassword(password, userID) {
+    return new Promise((resolve, reject) => {
+      DB.execute(
+        `UPDATE userdetails SET password = "${password}" WHERE userID = "${userID}"`
+      )
+        .then(([result]) => {
+          resolve(result);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  
+};
