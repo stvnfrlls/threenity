879804f0b3aad5f793b9c011f7793f6adb4b801a
stvnfrlls <stvnfrlls@gmail.com>
2022-10-05 22:05:12 +0800

    added routes to index

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,6 @@ const express = require("express");
 const controller = require("../controllers/controller");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
-});
+router.get("/", controller.renderIndexPage);
 
 module.exports = router;
